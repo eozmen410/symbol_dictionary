60f389d83942f09d3aa4edb664047df696c3a73c
eozmen410 <eo2419@columbia.edu>
2019-07-17 22:37:40 -0400

    using bootstrap instead of table element, fixed hide and shows

--- dict.js
+++ dict.js
@@ -4,6 +4,7 @@ var nounCount = 0;
 var adjCount = 0;
 var allCount = 0;
 var concepts = [];
+var total = 0;
 var events = ['winter', 'summer', 'spring', 'fall','christmas', 'new years', 'halloween', 'easter','4th of july','valentines day','hanukkah', 'chinese new years','super bowl', 'day of the dead','st patricks day', 'ramadan','mother\'s day', 'father\'s day','thanksgiving','cinco de mayo']
 $(document).ready(function() {
     console.log("document ready, all_data:");
@@ -14,16 +15,30 @@ $(document).ready(function() {
     console.log(concepts)
     // $("#bar").css('display', 'none')
     load_concepts_array()
-    loadImages("img_grid", active_type);
+    // loadImages("img_grid", active_type);
+
+    var hash = window.location.hash;
+		// $('#myTab a[href="' + hash + '"]').tab('show');
+    if (hash.length > 1) {
+        console.log(hash)
+        var w_id = hash.split('_')[0]
+        w_id = w_id.substr(1)
+        console.log(w_id)
+        setActive(w_id)
+    }
+
+    // display_symbols(active_type)
 
     $("#header").click(function(){
         $("#message").hide()
         console.log('clicked on home')
         // $("#btns").removeClass('invisible')
+        $("#btns").hide()
         $("#search_btns").empty()
         // loadHome()
         active_type="none"
-        loadImages("img_grid", active_type);
+        // loadImages("img_grid", active_type);
+        display_symbols(active_type)
     })
 
     $("#search_btn").click(function(){
@@ -35,10 +50,12 @@ $(document).ready(function() {
 
     $("#search_in").keyup(function(){
 
-         var input =$("#search_in").val() 
+        var input =$("#search_in").val() 
         console.log(input)
         search(input)
     })
+
+    //add hash val load images
 });
 
 
@@ -60,34 +77,6 @@ function search(word) {
 
 }
 
-function load_search_results(arr) {
-    // $("verb_btns").empty()
-    // $("noun_btns").empty()
-    // $("adjective_btns").empty()
-    // $("#btns").addClass('invisible')
-    empty_concepts()
-    $("#search_btns").empty()
-    $("#search_btns").removeClass('invsible')
-    // $("#search_btns").removeClass('invsible')
-    setActive("none")
-    for(var i=0; i<arr.length;i++) {
-        var id = arr[i]['id'];
-        var name = arr[i]['concept'];
-        var pos = all_data[id]['partOfSpeech'];
-        var link = id + '_' + name;
-        var btn = $("<button class='btn homeBtn'>"+ capitalFirstLetter(name) +" ("+pos+")"+ "</button>")
-        $(btn).attr('id', id)
-        $(btn).attr('href', '#'+link)
-        $(btn).click(function(){
-            setActive(this.id)
-            $("#search_btns").empty()
-            // $("#bar").css('display', 'block')
-        })
-        var btndiv = $("<div class='row'>")
-        $(btndiv).append(btn)
-        $("#search_btns").append(btndiv)
-    }
-}
 
 function load_concepts_array() {
     for (var key in all_data) {
@@ -108,6 +97,8 @@ function empty_concepts() {
 }
 
 function loadHome(arr) {
+    $("#images").hide()
+    $("#btns").show()
     console.log(arr)
     $("#verb_btns").empty()
     $("#verb_btns").append('<div align="center" class="col-header">Verbs</div><br>')
@@ -134,6 +125,7 @@ function loadHome(arr) {
             setActive(this.id)
             // $("#btns").empty()
             // $("#btns").addClass('invisible')
+            $("#btns").hide()
             // $("#bar").css('display', 'block')
             empty_concepts()
         })
@@ -170,6 +162,7 @@ function addConcept(id, name, partOfSpeech) {
     $(conceptOption).attr('id',  id);
     $(conceptOption).click(function() {
         console.log(this.id);
+        $("#btns").hide()
         // $("#btns").addClass('invisible')
         // $("#search_btns").empty()
         empty_concepts()
@@ -202,52 +195,115 @@ function capitalFirstLetter(name) {
     return name.charAt(0).toUpperCase() + name.slice(1)
 }
 
-function loadImages(table_id, id) {
-    console.log('creating image grid...')
-    console.log(id)
-    if (id !== "none") {
-        $("#message").show()
-        var symbol_list = all_data[id]["symbols"]
-        var count = 0;
-        var concept = all_data[id]["concept_name"]
-        concept =  capitalFirstLetter(concept);
-        // clear table first.
-        $('#' + table_id + ' tr').remove();
-        var table = document.getElementById(table_id);
-        row_num = 0;
-        var row = table.insertRow(row_num);
-        cell_num = 0;
-        for (i = 0; i < symbol_list.length; i++) {
-                        console.log(symbol_list.length)
-                        if(symbol_list[i]["deleted"].valueOf() === "False" ){
-            url = symbol_list[i]["imageURL"]
-            search_term = symbol_list[i]['term']
-            cell = row.insertCell(-1);
-            html = '<div class=\"parent\"> <img src=\"' + url + '\"/> <div  class=\"info\">' + search_term + '</div>';
+// function loadImages(table_id, id) {
+//     console.log('creating image grid...')
+//     console.log(id)
+//     $("#images").show()
+//     if (id !== "none") {
+//         $("#message").show()
+//         var symbol_list = all_data[id]["symbols"]
+//         var count = 0;
+//         var concept = all_data[id]["concept_name"]
+//         concept =  capitalFirstLetter(concept);
+//         // clear table first.
+//         $('#' + table_id + ' tr').remove();
+//         var table = document.getElementById(table_id);
+//         row_num = 0;
+//         var row = table.insertRow(row_num);
+//         cell_num = 0;
+//         for (i = 0; i < symbol_list.length; i++) {
+//                         console.log(symbol_list.length)
+//                         if(symbol_list[i]["deleted"].valueOf() === "False" ){
+//             url = symbol_list[i]["imageURL"]
+//             search_term = symbol_list[i]['term']
+//             cell = row.insertCell(-1);
+//             html = '<div class=\"parent\"> <img src=\"' + url + '\"/> <div  class=\"info\">' + search_term + '</div>';
 
-            // html = '<div class=\"parent\"> <img style=\'width: 350px; border: 2px solid white; padding: 3px; margin: 2px;\' src=\"' + url + '\"/> <div  class=\"info\">' + search_term + '</div>';
+//             // html = '<div class=\"parent\"> <img style=\'width: 350px; border: 2px solid white; padding: 3px; margin: 2px;\' src=\"' + url + '\"/> <div  class=\"info\">' + search_term + '</div>';
 
-            cell.innerHTML = html;
-                            count++;
-                        }
-            // make new row
-            if ((count ) % 3 == 0) {
-                row_num = row_num + 1;
-                row = table.insertRow(row_num);
-            }
+//             cell.innerHTML = html;
+//                             count++;
+//                         }
+//             // make new row
+//             if ((count ) % 3 == 0) {
+//                 row_num = row_num + 1;
+//                 row = table.insertRow(row_num);
+//             }
+//         }
+//         $("#symbolType").html(" [" + all_data[id]['partOfSpeech'] + "]")
+//         $("#symbolCount").html("# of symbols: " + count);
+//         $("#conceptName").html(concept);
+//     } else {
+//         $('#' + table_id + ' tr').remove();
+//         $("#message").hide()
+//         $("#symbolCount").html("");
+//         $("#conceptName").html("Pick a concept to start!");
+//     }
+// }
+
+function display_symbols(id) {
+    $("#images").show()
+    $("#btns").hide()
+    var symbol_list = all_data[id]["symbols"]
+    for (i = 0; i < symbol_list.length;) {
+        var counter = 0;
+        var imgRow = $("<div>")
+        $(imgRow).addClass("row")
+        $(imgRow).addClass("gridrow")
+    
+        while (i < symbol_list.length && counter < 5) {
+    
+          //if the symbol obj's deleted field is not True, display it
+          if (symbol_list[i]["deleted"].valueOf() == "False".valueOf()) {
+            symbol_added = true;
+            symbol_url = symbol_list[i]["imageURL"];
+    
+            var caption = $("<div>")
+            
+            var search_term = String(symbol_list[i]["term"]);
+            $(caption).html(search_term)
+            $(caption).addClass("term")
+            var term = $("<div>")
+            $(term).append(search_term)
+            $(term).addClass("term")
+         
+            var symbol = $("<div>")
+            $(symbol).addClass("col-md-2")
+            $(symbol).addClass("gridcolumns")
+            // wrap.append(symbol)
+            var symbol_img = $("<img>")
+            $(symbol_img).attr('src', symbol_url)
+            $(symbol_img).addClass("symbol")
+            $(symbol).append(symbol_img)
+            $(symbol).append(term)
+            $(imgRow).append(symbol)
+    
+            counter++;
+            total++;
+          }
+          i++;
         }
-        $("#symbolType").html(" [" + all_data[id]['partOfSpeech'] + "]")
-        $("#symbolCount").html("# of symbols: " + count);
-        $("#conceptName").html(concept);
-    } else {
-        $('#' + table_id + ' tr').remove();
-        $("#message").hide()
-        $("#symbolCount").html("");
-        $("#conceptName").html("Pick a concept to start!");
-    }
+        // console.log(imgRow)
+        $("#images").append(imgRow)
+      }
+
+      if(total == 1)
+      {
+        $("#symbolCount").empty()
+        $("#symbolCount").append("<div style='display: inline-block; vertical-align: middle;'> " +total + " symbol for this concept</div>")
+      }
+      else if (total > 1){
+        $("#symbolCount").empty()
+        $("#symbolCount").append("<div style='display: inline-block; vertical-align: middle;'> " +total + " symbols for this concept</div>")
+      }    
+      $("#symbolType").html(" [" + all_data[id]['partOfSpeech'] + "]")
+      //         $("#symbolCount").html("# of symbols: " + count);
+      var concept = all_data[id]["concept_name"]
+      $("#conceptName").html(concept);
 }
 
 function setActive(uid) {
     active_type = uid;
-    loadImages("img_grid",active_type);
+    // loadImages("img_grid",active_type);
+    display_symbols(uid)
 }	
